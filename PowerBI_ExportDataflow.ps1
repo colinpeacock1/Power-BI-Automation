509fa01a8415297b6e814eccc76195b8aaa323d9
Colin Peacock <colinpeacock@gmail.com>
2022-05-23 11:44:55 +0100

    third commit

--- PowerBI_ExportDataflow.ps1
+++ PowerBI_ExportDataflow.ps1
@@ -1,4 +1,96 @@
 # Connect to Power BI Service using OAuth
 Connect-PowerBIServiceAccount
 
-Export-PowerBIDataflow -WorkspaceId 5bc65217-59aa-4e90-a82b-12c706a52c21 -Id 33e96fae-7cb0-46c3-8fbb-19a823f768ca -OutFile .\dataflows\Sales.json
+function Get-WorkspaceId 
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [object] $Workspace
+    )
+
+    if (!([string]::IsNullOrEmpty($Workspace.Name)))
+    {
+        return $workspace.Id
+    }
+}
+
+function Get-DataflowId 
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [object] $Workspace,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [object] $Dataflow
+    )
+
+    if (!([string]::IsNullOrEmpty($Workspace.Name)))
+    {
+        return $workspace.Id
+    }
+}
+
+function Get-BIFlows {
+
+    write-host "--------------------------------------------------------------"
+    write-host "Starting... Get-PowerBIDataflow"
+    write-host "--------------------------------------------------------------"
+
+    $Dir = "C:\Users\PeacocCo\OneDrive - Coherent, Inc\Documents\PowerBIService\Workspaces\"
+    $Workspace = Get-PowerBIWorkspace –All
+    $DataSets =
+       ForEach ($workspace in $Workspace)
+        {
+        Write-Host $workspace.Name
+        ForEach ($dataset in (Get-PowerBIDataflow -WorkspaceId $workspace.Id))
+            {
+
+            [pscustomobject]@{
+
+                WorkspaceName = $Workspace.Name
+                WorkspaceID = $workspace.Id
+                DatasetName = $dataset.Name
+                DatasetID = $dataset.Id
+                DatasetOwner = $dataset.ConfiguredBy
+                
+                }
+            }
+        }
+
+       $OutDir = Join-Path -Path $Dir -ChildPath "MyWorkspaceDataflows.csv"
+       $DataSets | Export-Csv $OutDir -NoTypeInformation -Encoding UTF8
+
+        write-host "--------------------------------------------------------------"
+        write-host "Finished... Get-PowerBIDataflow"
+        write-host "--------------------------------------------------------------"
+}
+
+# Export-PowerBIDataflow -WorkspaceId 5bc65217-59aa-4e90-a82b-12c706a52c21 -Id 33e96fae-7cb0-46c3-8fbb-19a823f768ca -OutFile .\dataflows\Sales.json
+
+$Workspace = Get-PowerBIWorkspace –All
+ForEach ($workspace in $Workspace)
+{
+   [string] $WorkspaceId = Get-WorkspaceId -Workspace $workspace
+   Write-Output $workspace.Name $WorkspaceId
+   ForEach ($dataflow in (Get-PowerBIDataflow -WorkspaceId $workspace.Id))
+    {
+        $DataFlow = [pscustomobject]@{
+            WorkspaceName = $Workspace.Name
+            WorkspaceID = $workspace.Id
+            Name = $dataflow.Name
+            ID = $dataflow.Id
+            Owner = $dataflow.ConfiguredBy        
+        }
+
+        Write-Output $workspace.Name $WorkspaceId $dataflow.Name $dataflow.Id
+        $jsonPath = ".\dataflows\" + $DataFlow.WorkspaceName + "_" + $DataFlow.Name + ".json"
+        Export-PowerBIDataflow -WorkspaceId $WorkspaceId -Id $dataflow.Id -OutFile $jsonPath
+    }
+
+}
+
---
+++ PowerBI_RefreshHistoryAllWorkspaces.ps1
@@ -0,0 +1,75 @@
+# connect using credentials
+# $myCred = Get-Credential
+Connect-PowerBIServiceAccount #$myCred
+
+$Workspaces = Get-PowerBIWorkspace
+
+<#
+    1. Set-up export file
+    2. Remove any existing refresh history file
+#>
+
+$ExportFile =  'C:\Users\PeacocCo\OneDrive - Coherent, Inc\Documents\PowerBiRefreshHistory.csv'
+Remove-Item $ExportFile -Force -ErrorAction SilentlyContinue
+
+
+<#
+    1. Loop through each workspace where the credentials have read access
+    2. For each workspace get a dataset that is refreshable
+    3. Loop through datasets that are identified as refreshable
+    4. Set the URI to connect the REST API
+    5. Use the PowerBI Rest method to get the results of the refresh
+    6. For each refresh result get the key attributes and add to a new object
+    7. Write the results object to a csv file with name $ExportFile
+#>
+
+foreach($workspace in $Workspaces)
+
+{
+    try {
+
+        $DataSets = Get-PowerBIDataset -WorkspaceId $workspace.Id | where {$_.isRefreshable -eq $true}
+        foreach($dataset in $DataSets)
+
+        {
+            $URI = "groups/" + $workspace.id + "/datasets/" + $dataset.id + "/refreshes"
+            $Results = Invoke-PowerBIRestMethod -Url $URI -Method Get | ConvertFrom-Json
+            write-host $workspace.name $dataset.name
+
+            <# 
+            remove this comment to show the results information
+            write-host $Results.value
+             #>
+            foreach($result in $Results.value)
+
+            {
+                $errorDetails = $result.serviceExceptionJson | ConvertFrom-Json -ErrorAction SilentlyContinue
+                
+                $row = New-Object psobject
+                $row | Add-Member -Name "Workspace" -Value $workspace.Name -MemberType NoteProperty
+                $row | Add-Member -Name "Dataset" -Value $dataset.Name -MemberType NoteProperty
+                $row | Add-Member -Name "ConfiguredBy" -Value $dataset.ConfiguredBy -MemberType NoteProperty
+                $row | Add-Member -Name "refreshType" -Value $result.refreshType -MemberType NoteProperty
+                $row | Add-Member -Name "startTime" -Value $result.startTime -MemberType NoteProperty
+                $row | Add-Member -Name "endTime" -Value $result.endTime -MemberType NoteProperty
+                $row | Add-Member -Name "status" -Value $result.status -MemberType NoteProperty
+                $row | Add-Member -Name "errorCode" -Value $errorDetails.errorCode -MemberType NoteProperty
+                $row | Add-Member -Name "errorDescription" -Value $errorDetails.errorDescription -MemberType NoteProperty
+
+                write-host $row
+
+                $row | Export-Csv -Path $ExportFile -Append -Delimiter ';' -NoTypeInformation
+
+            }
+
+        }
+
+    } catch {
+
+        write-host "error..."
+
+    }
+}
+
+# Disconect the service account
+Disconnect-PowerBIServiceAccount
